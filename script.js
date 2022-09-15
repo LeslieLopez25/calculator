4452902000d492d768ececd19d843c7957c49c26
Leslie Lopez <leslie2515lopez@gmail.com>
2022-09-15 17:43:02 -0500

    Reset value and add decimal

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
-const cleanBtn = document.getElementById("clear-btn");
+const clearBtn = document.getElementById("clear-btn");
 
 function sendNumberValue(number) {
   // IF CURRENT DISPLAY VALUE IS 0, REPLACE IT, IF NOT ADD NUMBER
   const displayValue = calculatorDisplay.textContent;
   calculatorDisplay.textContent =
     displayValue === "0" ? number : displayValue + number;
+}
+
+function addDecimal() {
+  // IF NO DECIMAL, ADD ONE
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
 }
 
 // ADD EVENT LISTENERS FOR NUMBERS, OPERATORS, DECIMAL BUTTONS\
@@ -16,6 +23,14 @@ inputBtns.forEach((inputBtn) => {
   } else if (inputBtn.classList.contains("operator")) {
     inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains("decimal")) {
-    inputBtn.addEventListener("click", () => sendNumberValue());
+    inputBtn.addEventListener("click", () => addDecimal());
   }
 });
+
+// RESET DISPLAY
+function resetAll() {
+  calculatorDisplay.textContent = "0";
+}
+
+// EVENT LISTENER
+clearBtn.addEventListener("click", resetAll);
